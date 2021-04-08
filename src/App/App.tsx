c2a5559d3e91d8c56ddfed75374904aa8a81e4ff
Lucia <luciabsep18@gmail.com>
2021-04-08 16:10:26 -0300

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+/*plop se utiliza con: npx plop component*/
+
+const App: React.FC = () => {
+  return (
+    <main>
+      <h1>Hola</h1>
+    </main>
+  )
+}
+
+export default App
